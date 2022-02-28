1678c21b9e3b8b9e591ba82bc759f136ef51a636
Szymaniak <szymanski.damian.1999@gmail.com>
2022-02-28 14:01:24 +0100

    28.02.2022

---
+++ WypozyczalniaFilmowa/Models/Kategoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WypozyczalniaFilmowa.Models
+{
+    public class Kategoria
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public string Opis { get; set; }
+        public ICollection<Film> Film { get; set; }
+        
+        
+    }
+}
